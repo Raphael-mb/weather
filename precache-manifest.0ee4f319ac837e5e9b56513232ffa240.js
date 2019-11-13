3f711f950b6ddfdd8a86b4e7b7be2a2d66f94cb0
Raphael Bradbury <raphybradbury@gmail.com>
2019-11-14 00:39:47 +0200

    Updates

---
+++ precache-manifest.0ee4f319ac837e5e9b56513232ffa240.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ad87753e3bcccce0ac125e83b906383",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "c23eb20387d5ce347dbe",
+    "url": "/weather/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "4d4bc7c0dc0df6c4841c",
+    "url": "/weather/static/css/main.61c3d35f.chunk.css"
+  },
+  {
+    "revision": "c23eb20387d5ce347dbe",
+    "url": "/weather/static/js/2.211b116e.chunk.js"
+  },
+  {
+    "revision": "4d4bc7c0dc0df6c4841c",
+    "url": "/weather/static/js/main.5c04e728.chunk.js"
+  },
+  {
+    "revision": "3cb3cde5c978189545b4",
+    "url": "/weather/static/js/runtime~main.6cff1e35.js"
+  }
+]);
